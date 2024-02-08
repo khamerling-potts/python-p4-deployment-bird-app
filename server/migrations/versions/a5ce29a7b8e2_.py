3c4c454e858aaf2358b93f028634efcfee9c8900
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-02-08 14:45:38 -0500

    production build

---
+++ server/migrations/versions/a5ce29a7b8e2_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a5ce29a7b8e2
+Revises: c93a14d1aa19
+Create Date: 2024-02-08 14:27:15.247036
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a5ce29a7b8e2'
+down_revision = 'c93a14d1aa19'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
